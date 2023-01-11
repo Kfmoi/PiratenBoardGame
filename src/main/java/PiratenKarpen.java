823657b6cff79b0e16736cb3c17437f6c1b34e4d
Khalid <faragk1@mcmaster.ca>
2023-01-11 12:52:19 -0500

    Worked on code

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,32 +1,59 @@
 import pk.Dice;
-import java.util.Scanner;
-
+import java.util.Random;
 public class PiratenKarpen {
 
 
-    class Player{
+    static class Player{
         public int points;
         public Dice dice;
 
         Player(){
             points = 0;
             dice = new Dice();
+            
         }
     }
     public static void main(String[] args) {
         int amountofgames = 42;
         int numberofskulls = 0;
         int maxrolls = 8;
+        int points = 0;
         Player player1 = new Player();
         Player player2 = new Player();
-        System.out.println(player1.dice.roll(););
+        Random selection = new Random();
+
+
+
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("I'm rolling a dice");
+        
+
+
         Dice myDice = new Dice();
         for (int i=0; i<amountofgames; i++){
             for(int j =0; j<maxrolls; j++){
+                System.out.println("I'm rolling a dice");
                 player1.dice.roll();
-            
+                if (player1.dice.roll().equals("GOLD")|| player1.dice.roll().equals("DIAMOND")){
+                    points += 1;
+                }
+                for (numberofskulls=0; player1.dice.roll().equals("SKULL"); numberofskulls++){
+                    if(numberofskulls ==3){
+                        System.out.println("This game is done");
+                    }
+                }
+            }
+            int rndmpick = selection.nextInt(((8-2)+1)+2);
+            for (int k=0; k<rndmpick; k++){
+                System.out.println("I'm rolling a dice");
+                player1.dice.roll();
+                if (player1.dice.roll().equals("GOLD")|| player1.dice.roll().equals("DIAMOND")){
+                    points += 1;
+                }
+                for (numberofskulls=0; player1.dice.roll().equals("SKULL"); numberofskulls++){
+                    if(numberofskulls ==3){
+                        System.out.println("This game is done");
+                    }
+                }
             }
 
 
